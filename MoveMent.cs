5e65f160e0df86de59f1785769d039ea407f1974
khwangit <wang.k@husky.neu.edu>
2017-05-08 23:20:01 -0400

    onGround detection

--- MoveMent.cs
+++ MoveMent.cs
@@ -5,20 +5,35 @@ public class MoveMent : MonoBehaviour {
 
 	//public GameObject player;
 	private float lockPos = 0;
+	private bool onGround = false;
 	//public GameObject cam;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (onGround){
+			//if (Input.touchCount == 1){
+				transform.rotation = Quaternion.Euler(lockPos,transform.rotation.eulerAngles.y, lockPos);
+				transform.position = transform.position + transform.forward * 10f * Time.deltaTime;
+			//}
+		}
+	}
+		
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "Ground")
+		{
+			onGround = true;
+		}
+	}
 
-		//if (Input.touchCount == 1){
-		transform.rotation = Quaternion.Euler(lockPos,transform.rotation.eulerAngles.y, lockPos);
-		transform.position = transform.position + transform.forward * 10f * Time.deltaTime;
-		//}
-
+	void OnCollisionExit (Collision col){
+		if(col.gameObject.tag == "Ground"){
+			onGround = false;
+		}
 	}
 }
