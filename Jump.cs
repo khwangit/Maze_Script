ecce230f03eb8ce050493be72c78aa6480b99418
khwangit <wang.k@husky.neu.edu>
2017-06-29 00:11:17 -0400

    completed v2
    change timer to bar

--- Jump.cs
+++ Jump.cs
@@ -10,7 +10,9 @@ public class Jump : MonoBehaviour {
 	public TextMesh timerText;
 	public GameObject sky;
 	public float jumpforce = 30f;
+	public Transform cbar;
 	private bool onGround = false;
+	public float ssc;
 
 	public Transform other;
 
@@ -18,33 +20,36 @@ public class Jump : MonoBehaviour {
 	void Awake () {
 		coolDown = coolDown;
 		sky.SetActive(false);
-		timerText.text = Mathf.RoundToInt (coolDown).ToString();
+		//timerText.text = Mathf.RoundToInt (coolDown).ToString();
+		cbar.localScale = new Vector3 (coolDown*0.005f , 1f, 0.01f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (coolDown <= 0f) {
 			sky.SetActive (true);
-			timerText.text = "Jump Ready!";
+			timerText.text = "Ready to Jump!";
 			/*if(Input.GetMouseButtonDown(0)){
 				clickToJump();
 			}*/
 		} else if (onGround == true) {
 			coolDown -= Time.deltaTime;
-			timerText.text = Mathf.RoundToInt (coolDown).ToString();
+			ssc = coolDown * 0.005f;
+			cbar.localScale = new Vector3 (ssc, 1f, 0.01f);
+			//timerText.text = Mathf.RoundToInt (coolDown).ToString();
 		}
 
 
-		if (other) {
+		/*if (other) {
 			float dist = Vector3.Distance(other.position, transform.position);
 			print("Distance to other: " + dist);
-		}
+		}*/
 	}
 
 	public void clickToJump(){
 		rb.velocity = new Vector3 (0f, jumpforce, 0f);
 		coolDown = resett;
-		timerText.text = coolDown.ToString ();
+		timerText.text = "";
 		sky.SetActive(false);
 	}
 
